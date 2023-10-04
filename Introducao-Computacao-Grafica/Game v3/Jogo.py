9885be276704d26962b87cae8bd856303dc65368
MariaLuiseB <marialuisebritto@gmail.com>
2023-10-03 23:09:51 -0300

    att

--- Introducao-Computacao-Grafica/Game v3/Jogo.py
+++ Introducao-Computacao-Grafica/Game v3/Jogo.py
@@ -4,7 +4,7 @@ from OpenGL.GLUT import *
 import pywavefront
 from pywavefront import visualization
 import numpy as np
-
+import random
 
 T1 = 0
 T2 = 0
@@ -18,17 +18,17 @@ velocidade = 0.8
 
 # Lista das Posições dos Ovnis 
 ovni_posicoes = [
-    (-5.0, 0.0, -1.7),
-    (3.0, 0.0, -2.5),
-    (-1.0, 0.0, 3.2),
-    (1.0, 0.0, 1.0),
+    (-5.0, -1.7, 0.0),
+    (3.0, -2.5, 0.0),
+    (-1.0, 3.2, 0.0),
+    (1.0, 1.0, 0.0),
 ]
 # -------------- Desenha o Jogo --------------
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
 
-    movimentos()
+    movimentos_nave()
 
     # Configurar material roxo
     glColor3f(1.0, 1.0, 1.0)  # Cor branca
@@ -39,11 +39,10 @@ def display():
 
     glPushMatrix()
     # Ações em toda a nave
-    glTranslatef(T1, 0.0, T3)# posicao inicial da nave
+    glTranslatef(T1, T2, T3)# posicao inicial da nave
     # Corpo da nave
     # glColor3f(0.0, 1.0, 1.1) # cor da nave
-    glRotatef(-90, 0.0, 1.0, 0.0) # rotacao da nave
-    glRotatef(angulo, 0.0, 1.0, 0.0) # rotacao da nave
+    glRotatef(angulo, 0.0, 0.0, 1.0) # rotacao da nave
     visualization.draw(nave) # desenha a nave
     glPopMatrix()
 
@@ -55,64 +54,65 @@ def display():
         glScalef(0.2, 0.2, 0.2)  # Reduz o tamanho do ovni para 20%
         visualization.draw(ovni) # desenha o ovni
         glPopMatrix()
-    
-    # Desenha o planeta 
-    glPushMatrix()
-    glTranslatef(8.0, 0.0, -2.0) # posicao inicial do planeta
-    glRotatef(180, 0.0, 0.5, 1.0) # rotacao do planeta
-    glScalef(2.0, 2.0, 2.0) # tamanho do planeta 
-    visualization.draw(planeta) # desenha o planeta
-    glPopMatrix()
 
     # -------------- eixo X --------------
     glBegin(GL_LINES)
-    glColor3f(1.0, 0.0, 0.0)
+    glColor3f(1.0, 0.0, 0.0) # cor do eixo X é vermelho
     glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(10.0, 0.0, 0.0)
     glEnd()
 
     glBegin(GL_LINES)
-    glColor3f(1.0, 0.0, 0.0)
+    glColor3f(1.0, 0.0, 0.0) # cor do eixo X é vermelho
     glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(-10.0, 0.0, 0.0)
     glEnd()
     
     # -------------- eixo Y --------------
     glBegin(GL_LINES)
-    glColor3f(0.0, 1.0, 0.0)
+    glColor3f(0.0, 1.0, 0.0) # cor do eixo Y é verde
     glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, 10.0, 0.0)
     glEnd()
 
     glBegin(GL_LINES)
-    glColor3f(0.0, 1.0, 0.0)
+    glColor3f(0.0, 1.0, 0.0) # cor do eixo Y é verde
     glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, -10.0, 0.0)
     glEnd()
     
     # -------------- eixo Z --------------
     glBegin(GL_LINES)
-    glColor3f(0.0, 0.0, 1.0)
+    glColor3f(0.0, 0.0, 1.0) # cor do eixo Z é azul
     glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, 0.0, 10.0)
     glEnd()
 
     glBegin(GL_LINES)
-    glColor3f(0.0, 0.0, 1.0)
+    glColor3f(0.0, 0.0, 1.0) # cor do eixo Z é azul
     glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, 0.0, -10.0)
     glEnd()
 
-    # -------------- CHÃO --------------
-    # glBegin(GL_POLYGON)
-    # glColor3f(0.0,0.0,0.2) # cor do chão
-    # glVertex3f(10,-1,9) # ponto do 1º quadrante (x,z)
-    # glVertex3f(10,-1,-50) # ponto do 2º quadrante (x,z)
-    # glVertex3f(-10,-1,-50) # ponto do 3º quadrandte (x,z)
-    # glVertex3f(-10,-1,9) # ponto do 4º quadrandte (x,z)
-    # glEnd()
+    # -------------- FUNDO -------------- 
+    # glPushMatrix()
+    # glTranslatef(0.0, 0.0, -5.0)
+    # visualization.draw(fundo)
+    # glPopMatrix()
 
     glutSwapBuffers()
+
+def Keys(key, x, y):
+    global cima, baixo, esquerda, direita, velocidade
+    
+    if(key == GLUT_KEY_LEFT ): 
+        esquerda = 1
+    elif(key == GLUT_KEY_RIGHT ): 
+        direita = 1
+    elif(key == GLUT_KEY_UP ): 
+        cima = 1
+    elif(key == GLUT_KEY_DOWN ):
+        baixo = 1
 
 def KeysUp(key, x, y):
     global cima, baixo, esquerda, direita, velocidade
@@ -124,58 +124,48 @@ def KeysUp(key, x, y):
         cima = 0
     elif(key == GLUT_KEY_DOWN ):
         baixo = 0
-        velocidade = 0.8
 
-def Keys(key, x, y):
-    global T1
-    global T3
-    global angulo, cima, baixo, esquerda, direita, velocidade
-    
-    if(key == GLUT_KEY_LEFT ): 
-            esquerda = 1
-    elif(key == GLUT_KEY_RIGHT ): 
-            direita = 1
-    elif(key == GLUT_KEY_UP ): 
-           cima = 1
-    elif(key == GLUT_KEY_DOWN ):
-            baixo = 1
-
-def movimentos():
-    global T1
-    global T3
-    global angulo
-    global cima, baixo, esquerda, direita, velocidade
+# Função de atualização da nave
+def movimentos_nave():
+    global angulo, cima, esquerda, direita, velocidade
 
     if esquerda == 1:
         angulo += 5.0
+        if velocidade > 1.0:
+            velocidade -= 0.02
     if direita == 1:
         angulo -= 5.0
+        if velocidade > 1.0:
+            velocidade -= 0.02
     if cima == 1:
-        if (velocidade < 4.0):
-            velocidade += 0.03
-        T1 -= (0.15 * np.cos(np.radians(angulo))) * velocidade
-        T3 -= (0.15 * np.sin(np.radians(angulo))) * velocidade
-    
+        move_cima_nave()
     if baixo == 1:
-        if (velocidade > 4.0):
-            velocidade -= 0.03
-        T1 += (0.15 * np.cos(np.radians(angulo))) * velocidade
-        T3 += (0.15 * np.sin(np.radians(angulo))) * velocidade
-    
-    if cima == 0 and baixo == 0:
-        if (velocidade > 0.8):
-            velocidade -= 0.2
-            T1 += (0.15 * np.cos(np.radians(angulo))) * velocidade
-            T3 += (0.15 * np.sin(np.radians(angulo))) * velocidade
+        move_baixo_nave()
+
+
+# Função para mover a nave para frente
+def move_cima_nave():
+    global T1, T2, angulo, velocidade
+    if velocidade < 4.0:
+        velocidade += 0.02
+    T1 += (0.10 * np.cos(np.radians(angulo))) * velocidade
+    T2 += (0.10 * np.sin(np.radians(angulo))) * velocidade
+
+def move_baixo_nave():
+    global T1, T2, angulo, velocidade
+    if velocidade < 4.0:
+        velocidade += 0.02
+    T1 -= (0.10 * np.cos(np.radians(angulo))) * velocidade
+    T2 -= (0.10 * np.sin(np.radians(angulo))) * velocidade
 
 # -------------- animacao do jogo --------------        
 def animacao(value):
     glutPostRedisplay()
     glutTimerFunc(30, animacao,1)
 
-# def idle():
-#     global T1
-#     T1 -= 1
+def idle():
+    global T1
+    T1 -= 1
     
 # -------------- Câmera do Jogo --------------
 def resize(w, h): # função para a câmera acompanhar o movimento da nave
@@ -185,7 +175,7 @@ def resize(w, h): # função para a câmera acompanhar o movimento da nave
     gluPerspective(25.0, w/h, 1.0, 100.0) # define o angulo de visão
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    gluLookAt(0.0, 20.0, 5.0, # posição da câmera 
+    gluLookAt(0.0, 0.0, 20.0, # posição da câmera 
                 0.0, 0.0, 0.0, # onde a câmera vai apontar
                 0.0, 1.0, 0.0) # parte pra cima
 
@@ -228,9 +218,9 @@ glutInitWindowSize(1920, 1080)
 glutInitWindowPosition(100, 100)
 wind = glutCreateWindow("JOGO DE ASTEROIDES")
 init()
-ovni = pywavefront.Wavefront("Ovni.obj")
-nave = pywavefront.Wavefront("SpaceShip.obj")
-planeta = pywavefront.Wavefront("planeta.obj")
+ovni = pywavefront.Wavefront("ovni.obj")
+nave = pywavefront.Wavefront("nave.obj")
+fundo = pywavefront.Wavefront("fundo.obj")
 glutDisplayFunc(display)
 glutReshapeFunc(resize)
 glutTimerFunc(30,animacao,1)
